78d9425ebe5966493d7f6b80da618f63427e6e8a
cj <27069314+thelacking@users.noreply.github.com>
2025-02-08 16:47:47 -0800

    finished 5

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,9 @@
-const sumAll = function() {
-
+const sumAll = function(s, f) {
+    let num = s;
+    for (let index = s+1; index < f+1; index++) {
+    num += index;    
+    }
+    return num;
 };
 
 // Do not edit below this line
