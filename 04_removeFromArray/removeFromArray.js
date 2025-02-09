87392e804cf68cdc48d4ffc574fd0859a80701aa
cj <27069314+thelacking@users.noreply.github.com>
2025-02-08 16:47:40 -0800

    finished 4

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,12 @@
-const removeFromArray = function() {
-};
+const removeFromArray = function(arr,...args) {
+    const newarr = [];
+    arr.forEach((i) => {
+        if (!args.includes(i)) {
+          newarr.push(i);
+        }
+      });
+      return newarr;
+    };
 
 // Do not edit below this line
 module.exports = removeFromArray;
