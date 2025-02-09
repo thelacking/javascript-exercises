6baacc2b81bf00387114975470b4cc8259ec6553
cj <27069314+thelacking@users.noreply.github.com>
2025-02-08 17:03:47 -0800

    finished 7

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,9 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(input) {
+  return (input-+32)*(9/5);
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(input) {
+  return ((9/5)*input)+32;
 };
 
 // Do not edit below this line
