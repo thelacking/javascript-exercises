fd95ae1f32bfa6ee97f98bdd21ca4294c4255b47
cj <27069314+thelacking@users.noreply.github.com>
2025-02-11 15:18:02 -0800

    finished 8

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,33 @@
-const add = function() {
-	
+const add = function(x,y) {
+	return x + y;
 };
 
-const subtract = function() {
-	
+const subtract = function(x,y) {
+	return x - y;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+	let total = 0;
+    for (let i = 0; i < arr.length; i++) {
+        total += arr[i];
+    }
+    return total;
 };
 
-const multiply = function() {
-
+const multiply = function(x,y) {
+  return x * y;
 };
 
-const power = function() {
-	
+const power = function(x,y) {
+	Math.pow(x, y);
 };
 
-const factorial = function() {
-	
+const factorial = function(n) {
+	let fact = 1;
+  for (let i = 1; i <= n; i++) {
+    fact *= i;
+  }
+  return fact;
 };
 
 // Do not edit below this line
