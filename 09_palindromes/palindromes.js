f0ea584f1249c67e3b5a6d27643da178b7e96f1d
cj <27069314+thelacking@users.noreply.github.com>
2025-02-11 17:06:34 -0800

    finished 9

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,11 @@
-const palindromes = function () {
-
+const palindromes = function (a) {
+var string = a.split("").reverse().join("");
+if(string === a){
+    return true;
+}
+else{
+    return false;
+}
 };
 
 // Do not edit below this line
