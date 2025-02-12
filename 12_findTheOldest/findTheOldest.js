e6669eada7edf4ff694c4bcdf3ecce4444f61eda
cj <27069314+thelacking@users.noreply.github.com>
2025-02-11 17:40:48 -0800

    finished 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,16 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(list) {
+let oldestperson;
+let oldestAge= 0;
+    for (let index = 0; index < list.length; index++) {
+        let deathyear;
+        if(list[index].yearOfDeath === undefined) deathyear = 2025; else deathyear = list[index].yearOfDeath;
+        let age = deathyear-list[index].yearOfBirth;
+        if(age > oldestAge){
+            oldestperson = list[index];
+            oldestAge = age;
+        }
+    }
+    return oldestperson;
 };
 
 // Do not edit below this line
