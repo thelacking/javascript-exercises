41b11f33d6725cdf90d69820ac0094494ec50202
cj <27069314+thelacking@users.noreply.github.com>
2025-02-11 17:12:36 -0800

    finished 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,13 @@
-const fibonacci = function() {
-
-};
-
+const fibonacci = function(a) {
+    let count = a;
+    let firstPrev = 1;
+    let secondPrev = 0;
+    for (let i = 2; i <= count; i++) {
+        let current = firstPrev + secondPrev;
+        secondPrev = firstPrev;
+        firstPrev = current;
+    }
+    return firstPrev;
+}
 // Do not edit below this line
 module.exports = fibonacci;
